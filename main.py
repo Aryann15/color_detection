0ce7435ab8b2e8ef13345e889f08a0972d0ad37e
Aryann15 <rajlysm15@gmail.com>
2023-07-11 18:14:55 +0530

    masking

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import cv2 as cv
 
+
+from utils import get_limits
+
+yellow = [0,255,255]
 cap = cv.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
+    hsv= cv.cvtColor(frame , cv.COLOR_BGR2HSV)
 
-    cv.imshow('frame',frame)
+    lower_limit, upper_limit = get_limits(color=yellow)
+
+
+    mask = cv.inRange(hsv,lower_limit,upper_limit )
+
+
+    cv.imshow('frame',mask )
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
